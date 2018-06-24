dfd621cce65f45d81405bdf99232a59f850c88c9
Saketp96 <saketp96@utexas.edu>
2018-06-24 18:25:18 -0500

    Add Blockchain Class and Update Block class

--- main.js
+++ main.js
@@ -1,6 +1,35 @@
+const SHA256 = require('crypto-js/sha256');
 class Block{
-	constructor(index,timestamp,date,previousHash=''){
+	constructor(index,timestamp,data,previousHash=''){
+		this.index=index;
+		this.timestamp=timestamp;
+		this.date=date;
+		this.previousHash=previousHash;
+		this.hash = this.calculateHash
+	}
 
+	calculateHash(){
+		return SHA256(this.index+this.previousHash+this.timestamp+JSON.stringify(this.data)).toString();
 	}
 }
-// callhan
+
+class Blockchain{
+	constructor(){
+		this.chain = [this.createGenesisBlock()];
+	}
+
+	createGenesisBlock(){
+		return new Block(0,"06/24/2018","Genesis block","0")
+	}
+	getLatestBlock(){
+		return this.chain[this.chain.length -1];
+	}
+
+	addBlock(newBlock){
+		newBlock.previousHash = this.getLatestBlock.hash;
+		newBlock.hash = newBlock.calculateHash();
+		this.chain.push(newBlock);
+	}
+
+}
+
